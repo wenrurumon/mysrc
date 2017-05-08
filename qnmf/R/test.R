8990e313640e9402dcd38f8f3214d94874dcd4ce
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-08 14:41:47 -0500

    Update test.R

--- qnmf/R/test.R
+++ qnmf/R/test.R
@@ -1,9 +1,19 @@
 #Rmatrix
 
-rmat <- function(nrow,ncol,k,sparse=0.3,error=1){
+rmat <- function(nrow,ncol,k,sparse=0.3,error=1,fordeconv=FALSE){
   x <- matrix(runif(nrow*k,0,10),nrow,k)
   y <- matrix(runif(k*ncol,0,10),k,ncol)
   y[sample(length(y),length(y)*sparse)] <- 0
+  for(i in 1:ncol(y)){
+    if(all(y[,i]==0)){
+      yi <- y[,i]
+      yi[sample(length(yi),1)] <- 1
+      y[,i] <- yi
+    }
+  }
+  if(fordeconv){
+    y <- apply(y,2,function(x){x/sum(x)})
+  }
   a <- x %*% y
   error <- rnorm(length(a),mean=0,sd=sd(as.vector(a))) * error
   a <- a + error

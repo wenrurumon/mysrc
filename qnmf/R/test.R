212132561ea2e60648b802e9ba63637b6bae22f3
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-08 12:02:27 -0500

    Create test.R

---
+++ qnmf/R/test.R
@@ -0,0 +1,12 @@
+#Rmatrix
+
+rmat <- function(nrow,ncol,k,sparse=0.3,error=1){
+  x <- matrix(runif(nrow*k,0,10),nrow,k)
+  y <- matrix(runif(k*ncol,0,10),k,ncol)
+  y[sample(length(y),length(y)*sparse)] <- 0
+  a <- x %*% y
+  error <- rnorm(length(a),mean=0,sd=sd(as.vector(a))) * error
+  a <- a + error
+  a <- a * (a>0)
+  list(A=a,X=x,Y=y)
+}

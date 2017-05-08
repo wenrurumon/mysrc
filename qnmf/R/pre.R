9173770e6457e534bbc0e75c1b867452ab50b0cf
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-08 12:02:42 -0500

    Create pre.R

---
+++ qnmf/R/pre.R
@@ -0,0 +1,46 @@
+#PRE
+
+diag2 <- function(x){
+  if(length(x)==1){
+    return(as.matrix(x))
+  } else {
+    return(diag(x))
+  }
+}
+scale2 <- function(x){
+  apply(x,2,function(xi){
+    (xi-min(xi))/max(xi-min(xi))
+  })
+}
+ginv<-function(A){
+  A_svd<-fast.svd(A)
+  if(length(A_svd$d)==1){
+    A_inv<-A_svd$v%*%as.matrix(1/A_svd$d)%*%t(A_svd$u)
+  }else{
+    A_inv<-A_svd$v%*%diag(1/A_svd$d)%*%t(A_svd$u)
+  }
+  return(A_inv)
+}
+positive <- function(x){
+  x * (x>0)
+}
+qpca <- function(A,scale=T,rank=0){
+  if(scale){A <- scale(A)}
+  A.svd <- svd(A)
+  if(rank==0){
+    d <- A.svd$d
+  } else {
+    d <- A.svd$d-A.svd$d[min(rank+1,nrow(A),ncol(A))]
+  }
+  d <- d[d > 1e-10]
+  r <- length(d)
+  prop <- d^2; prop <- cumsum(prop/sum(prop))
+  d <- diag(d,length(d),length(d))
+  u <- A.svd$u[,1:r,drop=F]
+  v <- A.svd$v[,1:r,drop=F]
+  x <- u%*%sqrt(d)
+  y <- sqrt(d)%*%t(v)
+  z <- x %*% y
+  rlt <- list(rank=r,X=x,Y=y,Z=x%*%y,prop=prop)
+  return(rlt)
+}

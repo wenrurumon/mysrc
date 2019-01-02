3153da972b0c24af8702a72e646899c6d0925b26
進撃の雯雯老師 <askahu6@gmail.com>
2019-01-02 19:17:20 +0800

    Add files via upload

---
+++ MatrixCompletion/R/FL_demo.R
@@ -0,0 +1,23 @@
+#Processing data from base, idx and exe to JM
+
+jm <- function(pidi){
+  # print(k<<-k+1)
+  trate <- imrate(pidi)
+  i.base <- filter(base,pid==pidi)
+  i.idx <- filter(idx,pid==pidi)
+  i.idx <- sapply(tag.idx,function(i){
+    i <- filter(i.idx,item==i) %>%
+      group_by(pid,hours,item) %>% summarise(value=mean(value))
+    imspline(i$hours,i$value,i.base$hours,0.1,F,T)
+  })
+  i.exe <- filter(fexe,pid==pidi)
+  i.exe <- sapply(tag.exe,function(i){
+    i <- filter(i.exe,item==i)
+    imrange(i,i.base$hours)
+  })
+  rlt <- data.table(pid=pidi,com=i.base$com_or_not,diag_no=i.base$diag_no,
+                    orate=i.base$orate,hours=i.base$hours,
+                    i.idx,i.exe,trate=trate)
+  rlt
+}
+
